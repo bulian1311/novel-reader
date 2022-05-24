0bde58ba8086f66043bb7fe6f9613d7dbc474741
bulian1311 <bulian1311@gmail.com>
2022-05-24 13:41:44 +0300

    Add font comic neue.

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from "next/document";
 function Document() {
   return (
     <Html lang="ru">
-      <Head />
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Comic+Neue:wght@300;400;700&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
